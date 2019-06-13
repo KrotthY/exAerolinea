25656595299b81a8bd315a5b60504b54ff7a51b3
KrotthY <Carlospatriciovidal.C@gmail.com>
2019-06-13 12:59:52 -0400

    Update Aerolinea.py
    segunda pueba

--- Aerolinea.py
+++ Aerolinea.py
@@ -1,1 +1,8 @@
-print('Bienvenidos a la aerolinea humanos')
+print('Bienvenidos a la aerolinea humanos')print('Bienvenidos a la aerolinea humanos')
+
+print(' iniciando segunda prueba de git')
+
+'''SE pueden hacer hartas weas
+hay que ver si son utiles, por favor editen y 
+agregen mas cosas para ver como va funcionando el git'''
+print(' oe')
